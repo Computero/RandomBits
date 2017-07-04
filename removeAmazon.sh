e0a3428f773df2b0a0259c5f2d6e740a5c30fb7e
Computero <computerobz@outlook.com>
2017-07-04 14:29:21 -0500

    Added removeAmazon.sh

---
+++ removeAmazon.sh
@@ -0,0 +1,11 @@
+################################################################################
+# Name: removeAmazon.sh
+#
+# Download this file to your computer and do the following:
+# 1.) Ensure the script remains executable:     	chmod +x removeAmazon.sh
+# 2.) Execute the script afte each upgrade:		./removeamazon.sh
+################################################################################
+
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
